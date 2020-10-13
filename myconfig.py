d643a95519273c23e85b31f9009d71f811165140
Your Name <you@example.com>
2020-10-13 09:08:06 +0100

    Justin Car

--- myconfig.py
+++ myconfig.py
@@ -51,8 +51,8 @@
 # 
 # #STEERING
 # STEERING_CHANNEL = 1            #channel on the 9685 pwm board 0-15
-# STEERING_LEFT_PWM = 517          #pwm value for full left steering
-# STEERING_RIGHT_PWM = 257        #pwm value for full right steering
+STEERING_LEFT_PWM = 517          #pwm value for full left steering
+STEERING_RIGHT_PWM = 257        #pwm value for full right steering
 # 
 # #STEERING FOR PIGPIO_PWM
 # STEERING_PWM_PIN = 13           #Pin numbering according to Broadcom numbers
@@ -61,9 +61,9 @@
 # 
 # #THROTTLE
 # THROTTLE_CHANNEL = 0            #channel on the 9685 pwm board 0-15
-# THROTTLE_FORWARD_PWM = 280      #pwm value for max forward throttle
-# THROTTLE_STOPPED_PWM = 140      #pwm value for no movement
-# THROTTLE_REVERSE_PWM = 80      #pwm value for max reverse throttle
+THROTTLE_FORWARD_PWM = 465      #pwm value for max forward throttle
+THROTTLE_STOPPED_PWM = 345      #pwm value for no movement
+THROTTLE_REVERSE_PWM = 190      #pwm value for max reverse throttle
 # 
 # #THROTTLE FOR PIGPIO_PWM
 # THROTTLE_PWM_PIN = 18           #Pin numbering according to Broadcom numbers
